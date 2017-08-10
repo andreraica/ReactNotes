422ae81cb6767ede31c1722a96d3bcb29bbb7819
André Raiça Silva <fcamara_asilva@ltm.digital>
2017-08-10 10:06:32 -0300

    inciial

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './Notes.css';
+import Note from './Note'
+
+class App extends Component {
+ 
+  constructor(){
+    super();    
+    this.state = {notes: [], newNote:''};
+
+    //bind this do react
+    this.setNote = this.setNote.bind(this);
+    this.generateNotes = this.generateNotes.bind(this);
+    this.addNote = this.addNote.bind(this);
+
+    //inicializa lista
+    this.state.notes.push({description:'Anotação 1', actor:'Autor A', color:'yellow'});
+    this.state.notes.push({description:'Anotação 2', actor:'Autor B', color:'yellow'});
+  }
+  
+  generateNotes(){
+    return this.state.notes;
+  }
+
+  addNote(event){
+    event.preventDefault();
+    this.state.notes.push({description: this.state.newNote, actor:'', color:'yellow'});
+    this.setState({notes : this.state.notes})
+  }
+
+  setNote(evento){
+    this.setState({newNote:evento.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          <input type="text" id="note" onChange={this.setNote} /> 
+          <input type="button" id="btnAdd" value="Add" onClick={this.addNote}/> 
+        </p>
+        <p>
+          <input type="text" id="noteFilter" /> <a href="">Filtrar</a>
+        </p>
+
+        <Note generateNotesParam={this.generateNotes} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Note.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './Notes.css';
+
+class Note extends Component {
+  
+  constructor(){
+    super();
+    this.state = { notes : [] };
+  }
+
+  componentDidMount(){
+    this.setState({ notes: this.props.generateNotesParam() });   
+  }
+
+  render() {
+    return (
+      <div>
+
+        {this.state.notes.map(function(item){ 
+            return (
+            <div className="quote-container" key={item.description}> 
+              <i className="pin"></i>
+              <blockquote className={'note ' + item.color}>
+              {item.description}
+              <cite className="author">{item.actor}</cite>
+              </blockquote>
+            </div>
+            )
+        })};
+        
+      </div>
+    );
+  }
+}
+
+export default Note;
